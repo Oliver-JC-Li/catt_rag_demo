9d68ba678416b49e6f432fc6a6b05564426a1096
oliver <oli3@uw.edu>
2024-09-23 11:22:43 -0700

    Make changes to .py file to read the api key

--- catt_rag.py
+++ catt_rag.py
@@ -3,7 +3,6 @@ This is a RAG chatbot to answer research question regarding COVID-19 Anti-CD14 t
 part 1 and part 2 informed consent forms
 """
 
-from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
@@ -11,14 +10,12 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
-import os
 import streamlit as st
 
-load_dotenv()
-os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
+openai_api_key = st.secrets["OPENAI_API_KEY"]
 
 # Load the document directory
-loader = PyPDFDirectoryLoader("./consent_forms/catt")
+loader = PyPDFDirectoryLoader("./catt")
 docs = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=100)
 doc_chunks = text_splitter.split_documents(docs)
@@ -27,7 +24,7 @@ doc_chunks = text_splitter.split_documents(docs)
 vector_db = FAISS.from_documents(doc_chunks, embedding=OpenAIEmbeddings())
 
 # Define LLM and prompt to use
-llm = ChatOpenAI(model='gpt-4o-mini')
+llm = ChatOpenAI(model='gpt-4o-mini', api_key=openai_api_key)
 
 prompt = ChatPromptTemplate.from_template(
     """
